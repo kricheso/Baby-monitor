1ae86bc88946b50efaeb7bc605fbd2379b26fcf1
Kousei Richeson <kousei.richeson@gmail.com>
2021-01-31 07:08:35 -0700

    First Commit

---
+++ CodingAssignment2021/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  CodingAssignment2021
+//
+//  Created by Jeff Huang on 1/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Coding Assignment 2021
